e173d8dac40de01f3a2513ce41b20926047dd2f4
s1270216aizu <taikiseto11@gmail.com>
2024-07-28 15:21:48 +0900

    Updated some blogs

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 
 class User(models.Model):
  name = models.CharField(max_length=20)
- login = models.CharField(max_length=20)
+ login = models.CharField(max_length=20) # login name
  password = models.CharField(max_length=255, default='defaultpassword') 
  icon = models.ImageField(default='blog/icon/default_icon.png',upload_to = 'blog/icon/')
  profile = models.TextField(default='Nice to meet you!')
